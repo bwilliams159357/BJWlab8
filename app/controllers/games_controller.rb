738c2433efa06760769c25348b90ebdefb67c245
Brian Williams <bwilliams159357@yahoo.com>
2011-12-09 21:57:47 -0600

    Restricted some things to admin only

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,82 +2,110 @@ class GamesController < ApplicationController
   # GET /games
   # GET /games.json
   def index
-    @games = Game.all
+    if !current_user.nil? && current_user.email == "admin@admin.com"
+      @games = Game.all
 
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @games }
+      respond_to do |format|
+        format.html # index.html.erb
+        format.json { render json: @games }
+      end
+    else
+      redirect_to root_path
     end
   end
 
   # GET /games/1
   # GET /games/1.json
   def show
-    @game = Game.find(params[:id])
+    if !current_user.nil? && current_user.email == "admin@admin.com"
+      @game = Game.find(params[:id])
 
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @game }
+      respond_to do |format|
+        format.html # show.html.erb
+        format.json { render json: @game }
+      end
+    else
+      redirect_to root_path
     end
   end
 
   # GET /games/new
   # GET /games/new.json
   def new
-    @game = Game.new
+    if !current_user.nil? && current_user.email == "admin@admin.com"
+      @game = Game.new
 
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @game }
+      respond_to do |format|
+        format.html # new.html.erb
+        format.json { render json: @game }
+      end
+    else
+      redirect_to root_path
     end
   end
 
   # GET /games/1/edit
   def edit
-    @game = Game.find(params[:id])
+    if !current_user.nil? && current_user.email == "admin@admin.com"
+      @game = Game.find(params[:id])
+    else
+      redirect_to root_path
+    end
   end
 
   # POST /games
   # POST /games.json
   def create
-    @game = Game.new(params[:game])
+    if !current_user.nil? && current_user.email == "admin@admin.com"
+      @game = Game.new(params[:game])
 
-    respond_to do |format|
-      if @game.save
-        format.html { redirect_to @game, notice: 'Game was successfully created.' }
-        format.json { render json: @game, status: :created, location: @game }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @game.errors, status: :unprocessable_entity }
+      respond_to do |format|
+        if @game.save
+          format.html { redirect_to @game, notice: 'Game was successfully created.' }
+          format.json { render json: @game, status: :created, location: @game }
+        else
+          format.html { render action: "new" }
+          format.json { render json: @game.errors, status: :unprocessable_entity }
+        end
       end
-    end
+    else
+      redirect_to root_path
+    end      
   end
 
   # PUT /games/1
   # PUT /games/1.json
   def update
-    @game = Game.find(params[:id])
+    if !current_user.nil? && current_user.email == "admin@admin.com"
+      @game = Game.find(params[:id])
 
-    respond_to do |format|
-      if @game.update_attributes(params[:game])
-        format.html { redirect_to @game, notice: 'Game was successfully updated.' }
-        format.json { head :ok }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @game.errors, status: :unprocessable_entity }
+      respond_to do |format|
+        if @game.update_attributes(params[:game])
+          format.html { redirect_to @game, notice: 'Game was successfully updated.' }
+          format.json { head :ok }
+        else
+          format.html { render action: "edit" }
+          format.json { render json: @game.errors, status: :unprocessable_entity }
+        end
       end
+    else
+      redirect_to root_path
     end
   end
 
   # DELETE /games/1
   # DELETE /games/1.json
   def destroy
-    @game = Game.find(params[:id])
-    @game.destroy
+    if !current_user.nil? && current_user.email == "admin@admin.com"
+      @game = Game.find(params[:id])
+      @game.destroy
 
-    respond_to do |format|
-      format.html { redirect_to games_url }
-      format.json { head :ok }
+      respond_to do |format|
+        format.html { redirect_to games_url }
+        format.json { head :ok }
+      end
+    else
+      redirect_to root_path
     end
   end
 end
